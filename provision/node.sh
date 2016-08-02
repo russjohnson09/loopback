8ec3b92c5be0b8d55e8c103b8be0e079abe8bf09
russjohnson09@gmail.com <russjohnson09@gmail.com>
2016-08-02 00:21:21 -0400

    added provision

---
+++ provision/node.sh
@@ -0,0 +1,18 @@
+
+#nodejs
+curl -sL https://deb.nodesource.com/setup | sudo bash - #http://stackoverflow.com/questions/12913141/message-failed-to-fetch-from-registry-while-trying-to-install-any-module
+sudo apt-get install nodejs -y
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install build-essential -y
+#sudo npm install -g request
+
+sudo npm cache clean -f
+
+sudo npm install -g n
+
+sudo n 6.2.2
+
+
+sudo rm /usr/bin/node
+
+sudo ln -s /usr/bin/node /usr/local/n/versions/node/6.2.2
